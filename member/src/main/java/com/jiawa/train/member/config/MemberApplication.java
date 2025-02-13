b2acafa467a4d8fa1be32c1c78283a99e90b0b0c
cc <1552607960@qq.com>
2025-02-13 14:36:21 +0800

    4.7 增加logaspect

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,4 +1,4 @@
-package org.jiawa.train.config;
+package com.jiawa.train.member.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@ComponentScan("org.jiawa")
+@ComponentScan("com.jiawa")
 public class MemberApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
