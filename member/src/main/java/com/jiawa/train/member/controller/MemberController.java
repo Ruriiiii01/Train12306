3ee38aad3e0f11e4de79275265bb496690e54ca6
cc <1552607960@qq.com>
2025-02-14 17:14:21 +0800

    4.19 集成校验框架，异常处理

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.jiawa.train.member.controller;
 
+import com.jiawa.train.common.resp.CommonResp;
+import com.jiawa.train.member.req.MemberRegisterReq;
 import com.jiawa.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +18,13 @@ public class MemberController {
     private MemberService memberService;
 
     @GetMapping("/count")
-    public Integer count() {
-        return memberService.count();
+    public CommonResp<Integer> count() {
+        return new CommonResp<>(memberService.count());
     }
 
     @PostMapping("/register")
-    public Long register(String mobile) {
-        return memberService.register(mobile);
+    public CommonResp<Long> register(@Valid MemberRegisterReq req) {
+        return new CommonResp<>(memberService.register(req));
     }
 
 }
--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -1,9 +1,12 @@
 package com.jiawa.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
+import com.jiawa.train.common.exception.BusinessException;
+import com.jiawa.train.common.exception.BusinessExceptionEnum;
 import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
+import com.jiawa.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
@@ -18,12 +21,13 @@ public class MemberService {
         return Math.toIntExact(memberMapper.countByExample(null));
     }
 
-    public long register(String mobile) {
+    public long register(MemberRegisterReq req) {
+        String mobile = req.getMobile();
         MemberExample memberExample = new MemberExample();
         memberExample.createCriteria().andMobileEqualTo(mobile);
         List<Member> members = memberMapper.selectByExample(memberExample);
         if(CollUtil.isNotEmpty(members)) {
-            throw new RuntimeException("手机号已注册");
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIT);
         }
         Member member = new Member();
         member.setMobile(mobile);
