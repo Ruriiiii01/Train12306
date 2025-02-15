ed842babb8934681bffaebea41e774645ae14a7d
cc <1552607960@qq.com>
2025-02-15 10:52:58 +0800

    5.9 后端接口修改为json输入

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -8,10 +8,7 @@ import com.jiawa.train.member.resp.MemberLoginResp;
 import com.jiawa.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -31,12 +28,12 @@ public class MemberController {
     }
 
     @PostMapping("/send-code")
-    public CommonResp<String> sendCode(@Valid MemberSendCodeReq req) {
+    public CommonResp<String> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
         return new CommonResp<>(memberService.sendCode(req));
     }
 
     @PostMapping("/login")
-    public CommonResp<MemberLoginResp> login(@Valid MemberLoginReq req) {
+    public CommonResp<MemberLoginResp> login(@Valid @RequestBody MemberLoginReq req) {
         return new CommonResp<>(memberService.login(req));
     }
 
